89f9a20361994bbe3ba267fd24138e651de8f331
Kenneth Zhao <kzhao@solution-soft.com>
2016-02-25 19:05:55 -0600

    fix shared lib issu

--- fixcerts.sh
+++ fixcerts.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
+BUNDLE="/var/ssl/certs/ca-certificates.crt"
 ME=`whoami`
 
 if [ $ME = "root" ]; then 
-  mkdir -p /etc/pki/tls/certs
-  curl http://curl.haxx.se/ca/cacert.pem -o /etc/pki/tls/certs/ca-bundle.crt
+  mkdir -p `dirname $BUNDLE`
+  curl http://curl.haxx.se/ca/cacert.pem -o $BUNDLE
   echo "Done!"
   exit 0
 fi
